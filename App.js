a96b81b232a1c483c19902220f1c54c8de92f412
masaru514 <415urasam@gmail.com>
2021-06-29 20:31:11 +0900

    update: heroku -> cloud functions

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-//ログイン処理
-const Discord = require('discord.js');
-const client = new Discord.Client();
-require('dotenv').config()
-const token = process.env.DISCORD_BOT_TOKEN
-
+const Discord = require('discord.js')
+const client = new Discord.Client()
 client.on('ready', () => {
-    client.user.setUsername("🍖 休憩ちゃん 🍖")
-    console.log('ready...');
-});
+  client.user.setUsername('🍖休憩ちゃん🍖(Cloud Functionsに転生しました！)')
+  console.log('ready...')
+})
 
-client.on('message', async message =>{
-  try{
+client.on('message', async (message) => {
+  try {
     let member = message.member
-    const foodArr = ['🍞', '🥐', '🥖', '🥯', '🧀', '🍖', '🍗', '🥩', '🍔', '🌭', '🥪', '🍲', '🍙', '🍚', '🍛', '🍜', '🍝', '🍣', '☕', '🥂', '🍽️' , '🍴']
-    const isBreak = foodArr.some(item => message.content === item)
+    const foodArr = [
+      '🍞',
+      '🥐',
+      '🥖',
+      '🥯',
+      '🧀',
+      '🍖',
+      '🍗',
+      '🥩',
+      '🍔',
+      '🌭',
+      '🥪',
+      '🍲',
+      '🍙',
+      '🍚',
+      '🍛',
+      '🍜',
+      '🍝',
+      '🍣',
+      '☕',
+      '🥂',
+      '🍽️',
+      '🍴',
+    ]
+    const isBreak = foodArr.some((item) => message.content === item)
     if (isBreak) {
-      if(member.nickname) {
+      if (member.nickname) {
         member = await member.setNickname(`${member.nickname}🍖`)
       } else {
         member = await member.setNickname(`${member.user.username}🍖`)
@@ -23,18 +42,24 @@ client.on('message', async message =>{
 
       // 多分邪魔なので今はコメントアウト
       // message.channel.send("休憩確認しました！いってらっしゃい〜")
-    } 
-    if(message.content === '🦴') {
-      const memberNameRemoveEmoji = member.nickname.replace(/🍖/g, "")
+    }
+    if (message.content === '🦴') {
+      const memberNameRemoveEmoji = member.nickname.replace(/🍖/g, '')
       member = await member.setNickname(`${memberNameRemoveEmoji}`)
       // 多分邪魔なので今はコメントアウト
       // message.channel.send("休憩終了を確認しました！おかえりなさい〜")
     }
   } catch (e) {
-    console.error(e); // It's always useful to log your errors.
+    console.error(e) // It's always useful to log your errors.
 
     // 多分邪魔なので今はコメントアウト
     // return message.channel.send("!WARNING 管理者権限を持っていると名前を変更出来ません！");
   }
-});
-client.login(token);
+})
+
+exports.restChecker = (req, res) => {
+  res.send('ready')
+  const token = process.env.DISCORD_BOT_TOKEN
+
+  client.login(token)
+}
