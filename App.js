9e2201c05f155c56e6aa97c5e7896d67fa98f636
masaru514 <415urasam@gmail.com>
2021-07-01 00:25:18 +0900

    update: appjs, readme file

--- App.js
+++ App.js
@@ -1,65 +1,63 @@
 const Discord = require('discord.js')
+// require('dotenv').config() // functions内では不要なためコメントアウト
 const client = new Discord.Client()
-client.on('ready', () => {
-  client.user.setUsername('🍖休憩ちゃん🍖(Cloud Functionsに転生しました！)')
-  console.log('ready...')
-})
-
-client.on('message', async (message) => {
-  try {
-    let member = message.member
-    const foodArr = [
-      '🍞',
-      '🥐',
-      '🥖',
-      '🥯',
-      '🧀',
-      '🍖',
-      '🍗',
-      '🥩',
-      '🍔',
-      '🌭',
-      '🥪',
-      '🍲',
-      '🍙',
-      '🍚',
-      '🍛',
-      '🍜',
-      '🍝',
-      '🍣',
-      '☕',
-      '🥂',
-      '🍽️',
-      '🍴',
-    ]
-    const isBreak = foodArr.some((item) => message.content === item)
-    if (isBreak) {
-      if (member.nickname) {
-        member = await member.setNickname(`${member.nickname}🍖`)
-      } else {
-        member = await member.setNickname(`${member.user.username}🍖`)
-      }
-
-      // 多分邪魔なので今はコメントアウト
-      // message.channel.send("休憩確認しました！いってらっしゃい〜")
-    }
-    if (message.content === '🦴') {
-      const memberNameRemoveEmoji = member.nickname.replace(/🍖/g, '')
-      member = await member.setNickname(`${memberNameRemoveEmoji}`)
-      // 多分邪魔なので今はコメントアウト
-      // message.channel.send("休憩終了を確認しました！おかえりなさい〜")
-    }
-  } catch (e) {
-    console.error(e) // It's always useful to log your errors.
-
-    // 多分邪魔なので今はコメントアウト
-    // return message.channel.send("!WARNING 管理者権限を持っていると名前を変更出来ません！");
-  }
-})
+const token = process.env.DISCORD_BOT_TOKEN
+client.login(token)
 
 exports.restChecker = (req, res) => {
   res.send('ready')
-  const token = process.env.DISCORD_BOT_TOKEN
+  console.log(token)
+  client.on('ready', () => {
+    client.user.setUsername('🍖休憩ちゃん🍖(Cloud Functionsに転生しました！)')
+    console.log('ready...')
+  })
 
-  client.login(token)
+  client.on('message', async (message) => {
+    try {
+      let member = message.member
+      const foodArr = [
+        '🍞',
+        '🥐',
+        '🥖',
+        '🥯',
+        '🧀',
+        '🍖',
+        '🍗',
+        '🥩',
+        '🍔',
+        '🌭',
+        '🥪',
+        '🍲',
+        '🍙',
+        '🍚',
+        '🍛',
+        '🍜',
+        '🍝',
+        '🍣',
+        '☕',
+        '🥂',
+        '🍽️',
+        '🍴',
+      ]
+      const isBreak = foodArr.some((item) => message.content === item)
+      if (isBreak) {
+        if (member.nickname) {
+          member = await member.setNickname(`${member.nickname}🍖`)
+        } else {
+          member = await member.setNickname(`${member.user.username}🍖`)
+        }
+
+        // 多分邪魔なので今はコメントアウト
+        // message.channel.send("休憩確認しました！いってらっしゃい〜")
+      }
+      if (message.content === '🦴') {
+        const memberNameRemoveEmoji = member.nickname.replace(/🍖/g, '')
+        member = await member.setNickname(`${memberNameRemoveEmoji}`)
+        // 多分邪魔なので今はコメントアウト
+        // message.channel.send("休憩終了を確認しました！おかえりなさい〜")
+      }
+    } catch (e) {
+      console.error(e) // It's always useful to log your errors.
+    }
+  })
 }
---
+++ README.md
@@ -0,0 +1,26 @@
+### 初めに
+
+npm install
+
+### ローカルで開発する場合
+
+1. \*\*環境変数を設定する
+   `App.js`内の下記コメントアウトを外す。
+
+```js
+require('dotenv').config()
+```
+
+Discord の Bot を起動するためには.env ファイルに TOKEN の記載が必要になります。
+
+2. \*\*Let's start!!
+
+```shell
+npm run start
+```
+
+3. \*\*終わったら deploy をする
+
+```shell
+npm run deploy
+```
